db30332ced563be29f225d0029618a133118716b
Estrela <estelapfarma@gmail.com>
2022-07-18 23:46:34 -0300

    first commit

---
+++ Alura Stickers/Alura Stickers/src/App.java
@@ -0,0 +1,17 @@
+import java.net.http.HttpClient;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+
+        // fazer uma conexção HTTP e buscar os top 250 filmes
+        String url "https://imdb-api.com/en/API/Top250Movies/k_0ojt0yvm";
+        
+        HttpClient.newHttpClient()
+        
+
+        // extrair só os dados que interessam (títlo, poster e classificação)
+
+        // exibir e manipular o dados
+    }
+}
